7237c4386f5a7a4b5674f7412639373e71f5c380
drt-hbr <86857306+hugobrt@users.noreply.github.com>
2026-08-13 20:36:12 +0200

    Add CustomCommands cog for Discord bot
    Implement custom commands functionality with JSON storage.

---
+++ cogs/customcommands.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import json
+import os
+
+def load_custom_commands():
+    if os.path.exists('commands.json'):
+        with open('commands.json', 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_custom_commands(data):
+    with open('commands.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+class CustomCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Ignore les bots et les messages privés
+        if message.author.bot or not message.guild:
+            return
+
+        # Vérifie si le message commence par "!" (notre préfixe)
+        if message.content.startswith('!'):
+            # Récupère le nom de la commande (ex: "!stream" -> "stream")
+            cmd_name = message.content[1:].split(' ')[0].lower()
+            
+            commands_data = load_custom_commands()
+            guild_id = str(message.guild.id)
+            
+            # Si le serveur a des commandes perso et que celle-ci existe
+            if guild_id in commands_data and cmd_name in commands_data[guild_id]:
+                cmd_data = commands_data[guild_id][cmd_name]
+                
+                try:
+                    color = int(cmd_data.get('color', '#5865F2').replace("#", ""), 16)
+                except:
+                    color = 0x5865F2
+                    
+                embed = discord.Embed(
+                    title=cmd_data.get('title', ''),
+                    description=cmd_data.get('description', ''),
+                    color=color
+                )
+                await message.channel.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(CustomCommands(bot))
